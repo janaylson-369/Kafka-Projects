c436cd2303757557c2be1678b2727e3882ae4208
Janailson <janailson.gomes@academico.ifpb.edu.br>
2026-05-29 04:17:55 -0300

    feat: melhora o cabeçalho adicionando ícone e estilização ao componente Navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { BsDiagram3 } from 'react-icons/bs'; 
 
 export default function Header() {
     return (
-        <Navbar bg="dark" data-bs-theme="dark">
+        
+        <Navbar bg="dark" data-bs-theme="dark" className="shadow-sm py-2">
             <Container>
-            <Navbar.Brand href="#home">KFA</Navbar.Brand>
+                {/* As classes d-flex, align-items-center e gap-2 alinham o ícone e o texto perfeitamente */}
+                <Navbar.Brand href="#home" className="d-flex align-items-center gap-2 fw-bold text-white fs-4">
+                    <BsDiagram3 className="text-primary" size={26} />
+                    <span>KAFKA Core</span>
+                </Navbar.Brand>
             </Container>
         </Navbar>
     );
-    }
+}
